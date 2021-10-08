ee6963568091bf46a6fbf5970c82a94d151fbe99
Lincoln Batista Páscoa <89076560+lpascoatd@users.noreply.github.com>
2021-10-08 19:20:56 +0100

    Create keybase.cmd

---
+++ keybase.cmd
@@ -0,0 +1,67 @@
+### Keybase proof
+
+I hereby claim:
+
+  * I am lpascoatd on github.
+  * I am lbpascoatd (https://keybase.io/lbpascoatd) on keybase.
+  * I have a public key ASCCX_A2luAHF13CTlQYLAuVAJVRu87NrXNJxnhX63SeJQo
+
+To claim this, I am signing this object:
+
+```json
+{
+  "body": {
+    "key": {
+      "eldest_kid": "012039b0e12316aea3602a7c24db4554d776dd69bf175859d6c87881e94c67f556120a",
+      "host": "keybase.io",
+      "kid": "0120825ff03696e007175dc24e54182c0b95009551bbcecdad7349c67857eb749e250a",
+      "uid": "0a41bb786128857ecab7e3ef1b15c919",
+      "username": "lbpascoatd"
+    },
+    "merkle_root": {
+      "ctime": 1633716936,
+      "hash": "9ddd57c15f45669d521fe9219fadb362da9fd646022c11972395623e870d1de8382466674771083754d21e9366bd7eb932756431da8c9ee2b0c5ddf55b726063",
+      "hash_meta": "fa3b77e32196b0db3535e729dc713118db3c1d1f16cb766a5a729da62ed23958",
+      "seqno": 20944425
+    },
+    "service": {
+      "entropy": "K7NharuPPUOdc96+trjxvL1M",
+      "name": "github",
+      "username": "lpascoatd"
+    },
+    "type": "web_service_binding",
+    "version": 2
+  },
+  "client": {
+    "name": "keybase.io go client",
+    "version": "5.8.0"
+  },
+  "ctime": 1633716987,
+  "expire_in": 504576000,
+  "prev": "5f5059f317a37ce4a0eb7cabd7db2e0d3057a8e408c49ad6d74fe1dab805040d",
+  "seqno": 12,
+  "tag": "signature"
+}
+```
+
+with the key [ASCCX_A2luAHF13CTlQYLAuVAJVRu87NrXNJxnhX63SeJQo](https://keybase.io/lbpascoatd), yielding the signature:
+
+```
+hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEggl/wNpbgBxddwk5UGCwLlQCVUbvOza1zScZ4V+t0niUKp3BheWxvYWTESpcCDMQgX1BZ8xejfOSg63yr19suDTBXqOQIxJrW10/h2rgFBA3EIDaj1FaSNU4bo2AMazboXsp+ygEEdcAPX7UrNT3/tMgBAgHCo3NpZ8RAnxDTv4ssyegHz6qJVfn7nPw+WEwDjF9UReDO3fvt0Eb9QS7rId9JC+1LKRbsb+SM3bNCegJ26aD/OVQsnoy9A6hzaWdfdHlwZSCkaGFzaIKkdHlwZQildmFsdWXEINQ7118uzSwH/X+bgx3UYJ1YDPTBW/8wpAUECtfz1mGJo3RhZ80CAqd2ZXJzaW9uAQ==
+
+```
+
+And finally, I am proving ownership of the github account by posting this as a gist.
+
+### My publicly-auditable identity:
+
+https://keybase.io/lbpascoatd
+
+### From the command line:
+
+Consider the [keybase command line program](https://keybase.io/download).
+
+```bash
+# look me up
+keybase id lbpascoatd
+```
